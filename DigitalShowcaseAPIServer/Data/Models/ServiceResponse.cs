838346bd521e2d12ded2dd43b544588ae2880acd
Eugene "Riketta" Chugaev <riketta@outlook.com>
2023-08-11 20:58:14 +0300

    Add ServiceResponse model

---
+++ DigitalShowcaseAPIServer/Data/Models/ServiceResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace DigitalShowcaseAPIServer.Data.Models
+{
+    public class ServiceResponse<T> where T : class
+    {
+        public T? Data { get; set; } = null;
+        public bool Success { get; set; } = true;
+        
+        /// <summary>
+        /// If <see cref="Success"/> set to false, consider <see cref="Message"/> as error message
+        /// </summary>
+        public string Message = string.Empty;
+    }
+}
